e846d4d18274b30bed23bff1d97fc013f2ed9b80
Mike Readhead <m.readhead@validus-ivc.co.uk>
2015-04-29 12:03:53 +0100

    Less code more functionality.

--- chatroom/chatroom.go
+++ chatroom/chatroom.go
@@ -2,27 +2,28 @@
 package chatroom
 
 import (
-	"bytes"
 	"crypto/rand"
 	"errors"
 	"io"
 	"log"
 	"net"
-	"os"
 
 	"golang.org/x/crypto/nacl/box"
 )
 
-const BUFFER_SIZE = 512
-
-var buf bytes.Buffer
-var logger = log.New(&buf, "Chat Logger: ", log.Llongfile)
+const BUFFER_SIZE = 32000
 
 // SecureReaderWriterCloser Combined reader/writer for use with shared keys
 type SecureReaderWriterCloser struct {
 	reader    *SecureReader
 	writer    *SecureWriter
-	transport io.Closer
+	transport io.ReadWriteCloser
+}
+
+type EncryptedMessage struct {
+	nonce   [24]byte
+	length  int32
+	message []byte
 }
 
 // Close Closes the transport used by the reader/writer
@@ -88,10 +89,9 @@ func (r *SecureReader) Read(message []byte) (int, error) {
 	var unencryptedMessage []byte
 	unencryptedMessage, ok := box.Open(unencryptedMessage, encryptedData[len(nonce):length], &nonce, r.sharedKey, r.privateKey)
 	length = length - box.Overhead - len(nonce)
-	log.Println("Unencrypted:" + string(unencryptedMessage))
 	if ok {
 		copy(message, unencryptedMessage)
-		return length, io.EOF
+		return length, err
 	}
 	return length, ErrorDecryption
 }
@@ -118,24 +118,16 @@ func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
 	return &SecureWriter{writer: w, privateKey: priv, sharedKey: pub}
 }
 
-// Dial generates a private/public key pair,
-// connects to the server, perform the handshake
-// and return a reader/writer.
-func Dial(addr string) (io.ReadWriteCloser, error) {
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	_, privateKey, peerKey := keyExchangeSendFirst(conn)
-	return NewSecureReaderWriterCloser(privateKey, peerKey, conn), nil
+func makeKeys() (*[32]byte, *[32]byte, *[32]byte) {
+	publicKey := new([32]byte)
+	privateKey := new([32]byte)
+	peerKey := new([32]byte)
+	return publicKey, privateKey, peerKey
 }
 
 // Don't use this. It's used for the demo only. If this goes wrong, give up here.
 func keyExchangeSendLast(transport io.ReadWriteCloser) (*[32]byte, *[32]byte, *[32]byte) {
-	publicKey := new([32]byte)
-	privateKey := new([32]byte)
-	peerKey := new([32]byte)
+	publicKey, privateKey, peerKey := makeKeys()
 	publicKey, privateKey, err := box.GenerateKey(rand.Reader)
 	if err != nil {
 		defer transport.Close()
@@ -156,9 +148,7 @@ func keyExchangeSendLast(transport io.ReadWriteCloser) (*[32]byte, *[32]byte, *[
 
 // Don't use this. It's used for the demo only.
 func keyExchangeSendFirst(transport io.ReadWriteCloser) (*[32]byte, *[32]byte, *[32]byte) {
-	publicKey := new([32]byte)
-	privateKey := new([32]byte)
-	peerKey := new([32]byte)
+	publicKey, privateKey, peerKey := makeKeys()
 	publicKey, privateKey, err := box.GenerateKey(rand.Reader)
 	if err != nil {
 		defer transport.Close()
@@ -177,91 +167,19 @@ func keyExchangeSendFirst(transport io.ReadWriteCloser) (*[32]byte, *[32]byte, *
 	return publicKey, privateKey, peerKey
 }
 
-var clients []net.Conn
-var history []string
-
-// Serve starts a secure echo server on the given listener.
-func Serve(l net.Listener) error {
-	for {
-		connect, err := l.Accept()
-		if err != nil {
-			return err
-		}
-		go func(transport net.Conn) {
-			_, privateKey, peerKey := keyExchangeSendFirst(transport)
-			rw := NewSecureReaderWriterCloser(privateKey, peerKey, transport)
-			buf := bytes.NewBuffer(make([]byte, bytes.MinRead))
-			_, err := buf.ReadFrom(rw)
-			if err != nil && err != io.EOF {
-				log.Fatal(err)
-				defer rw.Close()
-				panic(err)
-			}
-			clients = append(clients, transport)
-			log.Printf("Recieved Message\t[%s] from [%s]\n", buf.Bytes(), connect.RemoteAddr())
-			history = append(history, string(buf.Bytes()))
-			if len(history) > 10 {
-				history = history[len(history)-10:]
-			}
-			for _, oldMessage := range history {
-				log.Println("Server sending history " + oldMessage)
-				_, err = rw.Write([]byte(oldMessage))
-				if err != nil {
-					log.Fatal(err)
-					defer rw.Close()
-					panic(err)
-				}
-			}
-
-		}(connect)
-	}
-}
-
-func Client() {
-	// Client mode
-	if len(os.Args) != 3 {
-		log.Fatalf("Usage: %s <port> <message>", os.Args[0])
-	}
-	conn, err := Dial("localhost:" + os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := conn.Write([]byte(os.Args[2])); err != nil {
-		log.Fatal(err)
-	}
-	output := make(chan []byte)
-	errors := make(chan error)
-	go continousRead(conn, output, errors)
-	for {
-		select {
-		// This case means we recieved data on the connection
-		case data := <-output:
-			// Do something with the data
-			log.Printf("Recieved from Server %s\n", string(data))
-		// This case means we got an error and the goroutine has finished
-		case err := <-errors:
-			// handle our error then exit for loop
-			log.Panicln(err)
-		}
-	}
-}
-
 func continousRead(reader io.Reader, output chan []byte, errors chan error) {
-	//	buf := bytes.NewBuffer(make([]byte, 2048))
 	for {
 		// try to read the data
-
-		//		n, err := buf.ReadFrom(reader)
-		temp := make([]byte, 2048)
-		n, err := reader.Read(temp)
-		if err != ErrorDecryption && err != nil {
+		data := make([]byte, BUFFER_SIZE)
+		n, err := reader.Read(data)
+		if err != io.EOF && err != nil {
+			// send an error if it's encountered
 			errors <- err
+			return
 		}
 		// send data if we read some.
 		if n > 0 {
-			log.Printf("Bytes read %v\n", n)
-			output <- []byte(buf.Bytes())
+			output <- data
 		}
-
 	}
 }
